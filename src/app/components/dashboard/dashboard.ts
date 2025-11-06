07363fa91d827919db47e03f925dcf7c26eb1cca
¨John <gmzjohn7@gmail.com>
2025-11-05 22:55:34 -0400

    feat: update dashboard to use component views for home and users

--- src/app/components/dashboard/dashboard.ts
+++ src/app/components/dashboard/dashboard.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Home } from '../../view/home/home';
+import { Users } from '../../view/users/users';
 
 type View = 'home' | 'users';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, Home, Users],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
