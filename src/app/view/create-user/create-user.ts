9f0e13bb7220c26f13be06692fe2ae081fa41374
¨John <gmzjohn7@gmail.com>
2025-11-18 15:31:12 -0400

    feat: Add back button component and implement reactive form with styling for the create user view.

--- src/app/view/create-user/create-user.ts
+++ src/app/view/create-user/create-user.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
+import { BackButton } from '../../components/back-button/back-button';
 
 @Component({
   selector: 'app-create-user',
-  imports: [],
+  imports: [ReactiveFormsModule, BackButton],
   templateUrl: './create-user.html',
-  styleUrl: './create-user.css',
+  styleUrls: ['./create-user.css'],
 })
 export class CreateUser {
+  userForm = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+  });
 
+  onSubmit() {
+    console.log(this.userForm.value);
+  }
 }
