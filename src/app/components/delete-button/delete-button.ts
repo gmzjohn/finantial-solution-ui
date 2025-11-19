8ddfe7b2699631b51b821c28034aee3db66c831a
¨John <gmzjohn7@gmail.com>
2025-11-18 21:30:05 -0400

    feat: Add standalone edit and delete button components with SVG icons and integrate them into the table.

--- src/app/components/delete-button/delete-button.ts
+++ src/app/components/delete-button/delete-button.ts
@@ -2,10 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-delete-button',
+  standalone: true,
   imports: [],
   templateUrl: './delete-button.html',
   styleUrl: './delete-button.css',
 })
-export class DeleteButton {
-
-}
+export class DeleteButton {}
--- src/app/components/edit-button/edit-button.ts
+++ src/app/components/edit-button/edit-button.ts
@@ -2,10 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-edit-button',
+  standalone: true,
   imports: [],
   templateUrl: './edit-button.html',
   styleUrl: './edit-button.css',
 })
-export class EditButton {
-
-}
+export class EditButton {}
