76c6f990c7459e3e8bafc0ca8bdb8cfb49f3584c
¨John <gmzjohn7@gmail.com>
2025-11-18 21:16:03 -0400

    feat: add new edit button component

---
+++ src/app/components/edit-button/edit-button.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-button',
+  imports: [],
+  templateUrl: './edit-button.html',
+  styleUrl: './edit-button.css',
+})
+export class EditButton {
+
+}
